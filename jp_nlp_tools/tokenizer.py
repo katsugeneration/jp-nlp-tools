c0e1aef21b79576141d1e7125e90a9dae80fc643
katsugeneration <katsu.generation.888@gmail.com>
2020-06-07 23:10:06 +0900

    Add NgramTokenizer

--- jp_nlp_tools/tokenizer.py
+++ jp_nlp_tools/tokenizer.py
@@ -38,3 +38,33 @@ class MeCabTokenizer(object):
             tokenized (List[str]): tokenized stringss.
         """
         return self._tagger.parse(text).split()
+
+
+class NgramTokenizer(ITokenizer):
+    """Ngram tokenization.
+    
+    Args:
+        base_tokenizer (Tokinzer): base tokenizer useed for tokenize text before ngram concatenate.
+        n (int): ngram's n value.
+
+    """
+
+    def __init__(
+            self,
+            base_tokenizer: ITokenizer,
+            n :int = 2):
+        self._base_tokenizer = base_tokenizer
+        self._n = n
+
+    def tokenize(self, text: str) -> List[str]:
+        """Tokenize base tokenizer and concatenate ngram.
+
+        Args:
+            text (str): target text.
+
+        Return:
+            tokenized (List[str]): tokenized and ngram concatenated stringss.
+        """
+        tokens = self._base_tokenizer.tokenize(text)
+        length = len(tokens)
+        return ["".join(tokens[i:i+self._n]) for i in range(length-self._n+1)]
--- tests/test_tokenizer.py
+++ tests/test_tokenizer.py
@@ -1,5 +1,5 @@
 from nose.tools import ok_, eq_
-from jp_nlp_tools.tokenizer import MeCabTokenizer
+from jp_nlp_tools.tokenizer import MeCabTokenizer, NgramTokenizer
 
 
 class TestMeCabTokenizer:
@@ -11,3 +11,20 @@ class TestMeCabTokenizer:
         tokenizer = MeCabTokenizer()
         ret = tokenizer.tokenize('今日は晴れます')
         eq_(['今日', 'は', '晴れ', 'ます'], ret)
+
+
+class TestNgramTokenizer:
+    def test_init(self):
+        _tokenizer = MeCabTokenizer()
+        tokenizer = NgramTokenizer(_tokenizer)
+        ok_(tokenizer)
+
+    def test_tokenize(self):
+        _tokenizer = MeCabTokenizer()
+        tokenizer = NgramTokenizer(_tokenizer)
+        ret = tokenizer.tokenize('今日は晴れます')
+        eq_(['今日は', 'は晴れ', '晴れます'], ret)
+
+        tokenizer = NgramTokenizer(_tokenizer, 3)
+        ret = tokenizer.tokenize('今日は晴れます')
+        eq_(['今日は晴れ', 'は晴れます'], ret)
